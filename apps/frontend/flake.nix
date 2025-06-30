3fd38231f1a5a7fd0f1fbf3ab7baa6f2594210c0
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-06-30 15:43:52 +0200

    Setup new nix dev env

---
+++ apps/frontend/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pnpm
+          ];
+          shellHook = ''
+            # NOTE: this flags avoids ERR_OSSL_EVP_UNSUPPORTED
+            export NODE_OPTIONS=--openssl-legacy-provider
+          '';
+        };
+      }
+    );
+}
