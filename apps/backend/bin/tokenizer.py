1f01611dfe139dedf230e3f3a98d22c26f98d7bc
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-07-01 21:26:12 +0200

    Lint

--- apps/backend/bin/tokenizer.py
+++ apps/backend/bin/tokenizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S uv run --script
 
 import argparse
+
 # import logging
 import sys
 
@@ -15,25 +16,27 @@ from stanza.utils.conll import CoNLL
 #
 # logger = logging.getLogger(__name__)
 
+
 def core(model_path: str, source_path: str, target_path: str, format_type: str) -> None:
     if format_type == "conllu":
         tokenize_pretokenized = True
         doc = CoNLL.conll2doc(source_path)
     elif format_type == "plain":
         tokenize_pretokenized = False
-        with open(source_path, 'r') as f:
+        with open(source_path, "r") as f:
             doc = f.read()
     else:
         raise ValueError
 
     nlp = stanza.Pipeline(
-        lang='it',
+        lang="it",
         dir=model_path,
         download_method=None,
-        processors='tokenize,pos,lemma,depparse',
+        processors="tokenize,pos,lemma,depparse",
         tokenize_pretokenized=tokenize_pretokenized,
     )
     CoNLL.write_doc2conll(nlp(doc), target_path)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -44,6 +47,7 @@ def main():
     args = parser.parse_args()
 
     core(args.model_path, args.source_path, args.target_path, args.format_type)
+
 
 if __name__ == "__main__":
     main()
