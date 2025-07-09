afe1226defef369a26ce22a3ac4da0dc9fd97938
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-07-09 21:22:37 +0200

    Limit allowed methods

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=os.getenv("CORS_ALLOW_ORIGINS", "*").split(","),
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
